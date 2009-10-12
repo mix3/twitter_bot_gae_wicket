0819879ee76e0056a533c74eb33b635b4b6003e4
mix3 <himachocost333@hotmail.co.jp>
2009-10-12 17:36:30 +0900

    fix cron action

--- src/org/mix3/twitter_bot_gae_wicket/page/Bot.java
+++ src/org/mix3/twitter_bot_gae_wicket/page/Bot.java
@@ -1,19 +1,30 @@
 package org.mix3.twitter_bot_gae_wicket.page;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.wicket.PageParameters;
+import org.apache.wicket.RequestCycle;
+import org.apache.wicket.RestartResponseException;
+import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
+import org.apache.wicket.protocol.http.WebRequest;
 import org.mix3.twitter_bot_gae_wicket.service.Service;
 
 import twitter4j.TwitterException;
 
 import com.google.inject.Inject;
 
-public class Bot extends MyAbstractWebPage{
+public class Bot extends WebPage{
 	@Inject
 	private Service service;
 	
 	public Bot(PageParameters parameters){
 		super(parameters);
+		
+		HttpServletRequest request = ((WebRequest) RequestCycle.get().getRequest()).getHttpServletRequest();
+		if(request.getHeader("X-AppEngine-Cron") == null || !request.getHeader("X-AppEngine-Cron").equals("true")){
+			throw new RestartResponseException(Manage.class);
+		}
 		
 		add(new FeedbackPanel("feedback"));
 		try {
